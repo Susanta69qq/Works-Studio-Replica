626efd747f76aa41d9cef3bcda2d5ecada42b082
Susanta69qq <susantaray786@gmail.com>
2024-03-27 02:36:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+function loaderAnimation() {
+    var tl = gsap.timeline();
+
+    tl.to("#yellow1", {
+        top: "-100%",
+        delay: 0.5,
+        duration: 0.5,
+        ease: "expo.out"
+    })
+
+    tl.from("#yellow2", {
+        top: "100%",
+        delay: 0.5,
+        duration: 0.5,
+        ease: "expo.out"
+    }, "anim")
+
+    tl.to("#loader h1", {
+        delay: 0.6,
+        duration: 0.5,
+        color: "black"
+    }, "anim")
+
+    tl.to("#loader", {
+        display: "none"
+    })
+
+    tl.to("#loader", {
+        opacity: 0
+    })
+}
+loaderAnimation();
+
+function loco() {
+    const scroll = new LocomotiveScroll({
+        el: document.querySelector('#main'),
+        smooth: true
+    });
+
+    document.querySelector("#footer h2").addEventListener("click", () => {
+        scroll.scrollTo(0)
+    })
+
+    var elems = document.querySelectorAll(".elem");
+    var page2 = document.querySelector("#page2");
+    elems.forEach(function (elem) {
+        elem.addEventListener("mouseenter", function () {
+            var bgimg = elem.getAttribute("data-img")
+            page2.style.backgroundImage = `url(${bgimg})`
+        })
+    })
+}
+loco();
